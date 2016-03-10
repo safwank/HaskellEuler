bd23a53fade66426f81d83935f3b2a46b986916f
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2016-03-11 08:10:57 +1100

    Fix problem 4

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -1,11 +1,22 @@
 module Problem4 where
 
+import Test.HUnit
+import Data.List
+
 -- Problem 4: Largest palindrome product
 problem4 = largestPalindromeProduct 100 999
 
-largestPalindromeProduct min max = head $ filter (isPalindrome) $ map (product) $ nub $ map (take 2) $ permutations $ takeWhile (>=min) [max,max-1..]
+largestPalindromeProduct min max = head
+                                   $ filter (isPalindrome)
+                                   $ map (product)
+                                   $ nub
+                                   $ map (take 2)
+                                   $ permutations
+                                   $ takeWhile (>=min) [max,max-1..]
 
 isPalindrome x = let y = digits x
                  in y == reverse y
 
 digits n = map (\x -> read [x] :: Int) (show n)
+
+test4 = TestCase (assertEqual "problem #4" problem4 906609)
