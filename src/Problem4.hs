82feae30cf5216ee0ccd467dc295851a8beb3a3f
Safwan Kamarrudin <safwan.kamarrudin@myob.com>
2016-03-11 22:47:44 +1100

    Optimize problem 4

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -6,17 +6,19 @@ import Data.List
 -- Problem 4: Largest palindrome product
 problem4 = largestPalindromeProduct 100 999
 
-largestPalindromeProduct min max = head
+largestPalindromeProduct min max = maximum
                                    $ filter (isPalindrome)
                                    $ map (product)
-                                   $ nub
-                                   $ map (take 2)
-                                   $ permutations
-                                   $ takeWhile (>=min) [max,max-1..]
+                                   $ permutate 2
+                                   $ [max,max-1..min]
 
 isPalindrome x = let y = digits x
                  in y == reverse y
 
 digits n = map (\x -> read [x] :: Int) (show n)
+
+permutate _ [] = [[]]
+permutate 0 _ = [[]]
+permutate len xs = [x:y | x <- xs, y <- permutate (len-1) xs]
 
 test4 = TestCase (assertEqual "problem #4" problem4 906609)
