789e4204a75bf9a237d3462e72b280aeb2ecde93
Safwan Kamarrudin <safwan.kamarrudin@myob.com>
2016-03-11 23:20:42 +1100

    Refactor problem 4 to permutate using replicateM

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -1,7 +1,8 @@
 module Problem4 where
 
-import Test.HUnit
 import Data.List
+import Control.Monad
+import Test.HUnit
 
 -- Problem 4: Largest palindrome product
 problem4 = largestPalindromeProduct 100 999
@@ -9,16 +10,12 @@ problem4 = largestPalindromeProduct 100 999
 largestPalindromeProduct min max = maximum
                                    $ filter (isPalindrome)
                                    $ map (product)
-                                   $ permutate 2
+                                   $ replicateM 2
                                    $ [max,max-1..min]
 
 isPalindrome x = let y = digits x
                  in y == reverse y
 
 digits n = map (\x -> read [x] :: Int) (show n)
-
-permutate _ [] = [[]]
-permutate 0 _ = [[]]
-permutate len xs = [x:y | x <- xs, y <- permutate (len-1) xs]
 
 test4 = TestCase (assertEqual "problem #4" problem4 906609)
