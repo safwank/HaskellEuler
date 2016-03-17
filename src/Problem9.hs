237e096b3da0ac85d79c4a1391018333ed1f1d26
Safwan Kamarrudin <safwan.kamarrudin@myob.com>
2016-03-17 12:58:30 +1100

    Optimize problem 9

--- src/Problem9.hs
+++ src/Problem9.hs
@@ -10,13 +10,14 @@ pythagoreanTriplet total a = case triplets of
                                   [] -> pythagoreanTriplet total (a+1)
                                   _  -> head triplets
                              where triplets = filter (isPythagoreanTriplet total)
-                                               $ map (a:)
-                                               $ filter (\xs -> (head xs) < (last xs))
-                                               $ replicateM 2 [a+1..500]
+                                              $ map (\b -> a : b : [calculateC a b])
+                                              $ [a+1..500]
 
 isPythagoreanTriplet total xs = sum xs == total && pythagorean xs
 
 pythagorean (a:b:c:[]) = (c**2 == a**2 + b**2)
 pythagorean _ = False
+
+calculateC a b = sqrt $ a**2 + b**2
 
 test9 = TestCase (assertEqual "problem #9" problem9 31875000)
